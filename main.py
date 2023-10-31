00d564fea81b4f92f475ffc8fe079d0af2f2ce74
Hathor875 <krzysztofcieslik875@gmail.com>
2023-11-01 00:08:41 +0100

    add keyboardinterrupt except print

--- main.py
+++ main.py
@@ -62,5 +62,6 @@ if __name__ == '__main__':
             time.sleep(sleep_time)
             pass
     except KeyboardInterrupt:
+        print("Monitoring stopped by user.")
         observer.stop()
     observer.join()
